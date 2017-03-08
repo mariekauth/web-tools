878c5764fed11c9015bd3e59ce4e2c29a6684cff
shelbykauth <shelbykauth@gmail.com>
2017-03-08 09:36:21 -0500

    Put Property Type in Select rather than Input
    Re-do Property Type fields to be selected from a drop-down.
    Interface to add custom property types
    added 4 functions to ModelWriter namespace
    added related event trigger bindings to document.ready function
    updated ToDo list

--- scripts/model-writer.js
+++ scripts/model-writer.js
@@ -18,8 +18,70 @@ $(document).ready(function(){
         var ele = $(".args").first().clone();
         $(".args-container").append(ele);
     });
+    $("#language").on("change", function() {
+        switch ($(this).val()) {
+            case "php":
+                ModelWriter.defaultPropertyTypes = 
+                    ["int","bool","number","string","array","object"];
+            break;
+        }
+    }).trigger("change");
+    $("#types").on("change", function () {
+        $("input#add-type").val($(this).val());
+    });
+    ModelWriter.populatePropertyTypes();
 });
 var ModelWriter = {};
+ModelWriter.defaultPropertyTypes = ["int"
+                                   ,"bool"
+                                   ,"number"
+                                   ,"string"
+                                   ,"array"
+                                   ,"object"];
+ModelWriter.customPropertyTypes = [];
+ModelWriter.addPropertyType = function() {
+    var cTypes = ModelWriter.customPropertyTypes;
+    var dTypes = ModelWriter.defaultPropertyTypes;
+    var name = $("input#add-type").val();
+    if (cTypes.indexOf(name) != -1 || dTypes.indexOf(name) != -1) {
+        modal_errors("This property type already exists!");
+        return;
+    }
+    cTypes.push(name);
+    ModelWriter.populatePropertyTypes();
+}
+ModelWriter.removePropertyType = function() {
+    var cTypes = ModelWriter.customPropertyTypes;
+    var dTypes = ModelWriter.defaultPropertyTypes;
+    var name = $("input#add-type").val();
+    if (dTypes.indexOf(name) != -1) {
+        modal_errors("Cannot remove default property type!");
+        return;
+    }
+    if (cTypes.indexOf(name) == -1) {
+        modal_errors("Cannot remove property type that does not exist.");
+        return;
+    }
+    cTypes.splice(cTypes.indexOf(name),1);
+    ModelWriter.populatePropertyTypes();
+}
+ModelWriter.populatePropertyTypes = function() {
+    var selects = $("select[name='types'], select[name='arg_type[]']");
+    var mainOption1 = "<option value='' disabled selected>Property Types</option>";
+    var otherOption1 = "<option value='' disabled selected>Property Type</option>";
+    var typeList = "";
+    for (var type of ModelWriter.defaultPropertyTypes) {
+        typeList += "<option value='"+type+"'>"+type+"</option>";
+    }
+    typeList += "<option value='' disabled>-------</option>";
+    for (var type of ModelWriter.customPropertyTypes) {
+        typeList += "<option value='"+type+"'>"+type+"</option>";
+    }
+    selects.empty();
+    $("select[name='types']").append(mainOption1);
+    $("select[name='arg_type[]']").append(otherOption1);
+    selects.append(typeList);
+}
 ModelWriter.ucfirst = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
